67572964bd32f98d01f1f795b664a72d7ef5f30a
Jhonatha-Custodio <jhonatha.custodio@ibm.com>
2022-10-17 16:09:56 -0300

    BUILD: Create starter files

---
+++ src/main/java/br/com/ibm/welcomeboard/WelcomeboardApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibm.welcomeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ibm/welcomeboard/WelcomeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibm.welcomeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
