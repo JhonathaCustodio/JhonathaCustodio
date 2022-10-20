b06e3cc360190b5830e4828cd422401e7aaabad0
Jhonatha-Custodio <jhonatha.custodio@ibm.com>
2022-10-20 11:46:16 -0300

    REFACTOR: autowired increased on service and controller

--- src/main/java/br/com/ibm/welcomeboard/controller/WelcomeBoardController.java
+++ src/main/java/br/com/ibm/welcomeboard/controller/WelcomeBoardController.java
@@ -1,9 +1,14 @@
 package br.com.ibm.welcomeboard.controller;
 
+import br.com.ibm.welcomeboard.service.WelcomeBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/welcome")
 public class WelcomeBoardController {
+
+    @Autowired
+    private WelcomeBoardService service;
 
 }
--- src/main/java/br/com/ibm/welcomeboard/service/WelcomeBoardService.java
+++ src/main/java/br/com/ibm/welcomeboard/service/WelcomeBoardService.java
@@ -1,10 +1,15 @@
 package br.com.ibm.welcomeboard.service;
 
 
+import br.com.ibm.welcomeboard.repository.WelcomeBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class WelcomeBoardService {
+
+    @Autowired
+    private WelcomeBoardRepository repository;
 
 }
