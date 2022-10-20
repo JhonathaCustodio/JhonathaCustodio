ebca96f9539f93ec1029e34c57378452b0b11667
Jhonatha-Custodio <jhonatha.custodio@ibm.com>
2022-10-20 11:40:48 -0300

    FEATURE: Add Class Controller

---
+++ src/main/java/br/com/ibm/welcomeboard/controller/WelcomeBoardController.java
@@ -0,0 +1,9 @@
+package br.com.ibm.welcomeboard.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeBoardController {
+
+}
