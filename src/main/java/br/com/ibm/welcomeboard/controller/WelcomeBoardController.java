3a38544cb7172c9f7537b0dc212b8b6201f5c1ea
Jhonatha-Custodio <jhonatha.custodio@ibm.com>
2022-10-20 11:52:29 -0300

    FEATURE: Add POST from CRUD - CREATE

--- src/main/java/br/com/ibm/welcomeboard/controller/WelcomeBoardController.java
+++ src/main/java/br/com/ibm/welcomeboard/controller/WelcomeBoardController.java
@@ -1,8 +1,12 @@
 package br.com.ibm.welcomeboard.controller;
 
+import br.com.ibm.welcomeboard.domain.dto.WelcomeBoardDto;
 import br.com.ibm.welcomeboard.service.WelcomeBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/welcome")
@@ -11,4 +15,8 @@ public class WelcomeBoardController {
     @Autowired
     private WelcomeBoardService service;
 
+    @PostMapping
+    public ResponseEntity<WelcomeBoardDto> insert(@RequestBody WelcomeBoardDto welcomeBoardDto){
+        return new ResponseEntity<>(this.service.insertUser(welcomeBoardDto), CREATED);
+    }
 }
--- src/main/java/br/com/ibm/welcomeboard/service/WelcomeBoardService.java
+++ src/main/java/br/com/ibm/welcomeboard/service/WelcomeBoardService.java
@@ -1,9 +1,14 @@
 package br.com.ibm.welcomeboard.service;
 
 
+import br.com.ibm.welcomeboard.domain.converter.WelcomeBoardConverter;
+import br.com.ibm.welcomeboard.domain.dto.WelcomeBoardDto;
+import br.com.ibm.welcomeboard.entity.WelcomeBoardEntity;
 import br.com.ibm.welcomeboard.repository.WelcomeBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -11,5 +16,10 @@ public class WelcomeBoardService {
 
     @Autowired
     private WelcomeBoardRepository repository;
+
+    public WelcomeBoardDto insertUser(WelcomeBoardDto welcomeBoardDto) {
+        WelcomeBoardEntity welcomeBoardEntity = WelcomeBoardConverter.toEntity(Optional.of(welcomeBoardDto));
+        return WelcomeBoardConverter.toDto(Optional.ofNullable(repository.save(welcomeBoardEntity)));
+    }
 
 }
